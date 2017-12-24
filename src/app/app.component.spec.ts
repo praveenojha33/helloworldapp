a546954568f8138a19317e5e03d21c7b6554123e
praveenojha33 <praveenojha33@gmail.com>
2017-12-24 12:52:17 +0530

    Modified App First Time

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+//This file test for errors in app
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
@@ -13,15 +14,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'helloworldapp'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('helloworldapp');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to helloworldapp!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+//Import Component function from @angular/core module so that it can be used as decorator.
 import { Component } from '@angular/core';
-
+//Component decorator
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',//selector parameter passed to Component function.Selects the component to be targeted 
+  //here 'app-root'
+  templateUrl: './app.component.html',//template for the Component environments/index.html/'app-root'
+  styleUrls: ['./app.component.css']//CSS for the Component environments/index.html/'app-root'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'helloworldapp';//sends title for interpolated string in app.component.html
 }
