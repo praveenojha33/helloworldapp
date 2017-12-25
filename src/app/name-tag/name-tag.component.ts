afa12e9f1a36a6ed627259b2fae6be2639c009db
praveenojha33 <praveenojha33@gmail.com>
2017-12-25 15:04:22 +0530

    Added name-tags

---
+++ src/app/name-tag/name-tag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-name-tag',
+  templateUrl: './name-tag.component.html',
+  styleUrls: ['./name-tag.component.css']
+})
+export class NameTagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
